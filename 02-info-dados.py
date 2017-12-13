d8e71660c9f2fef77a01b200305f9c4818185535
Leonardo Alves <leunardosevla@gmail.com>
2017-12-13 17:12:38 -0200

    Adicionar 02-info-dados

---
+++ 02-info-dados.py
@@ -0,0 +1,9 @@
+# imports
+import pandas as pd
+import matplotlib.pyplot as plt 
+import numpy as np 
+from scipy.optimize import curve_fit
+from plot_erro import plot_erro
+
+data = pd.read_csv('./src/GlobalLandTemperaturesByCountry.csv')
+print(data.info())
